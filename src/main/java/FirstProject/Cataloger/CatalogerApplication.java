96fb212c09b1e4fa99cd138f96b207517762f807
tomaszciula <tomciuns@gmail.com>
2022-10-03 16:30:48 +0200

    catalog changed

---
+++ src/main/java/FirstProject/Cataloger/CatalogerApplication.java
@@ -0,0 +1,13 @@
+package FirstProject.Cataloger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogerApplication.class, args);
+	}
+
+}
