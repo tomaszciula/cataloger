1a6f8072ac9a0042c33d79ecb7c68105031ca9ca
Tomasz Kaczorkiewicz <bossandmaster69@gmail.com>
2022-11-25 00:56:02 +0100

    Add entity to Vinyl, add ManyToOne to Game and Vinyl

--- src/main/java/FirstProject/Cataloger/model/Vinyl.java
+++ src/main/java/FirstProject/Cataloger/model/Vinyl.java
@@ -2,15 +2,29 @@ package FirstProject.Cataloger.model;
 
 import lombok.*;
 
-@AllArgsConstructor
-@EqualsAndHashCode
+import javax.persistence.*;
+
+//@AllArgsConstructor
+//@EqualsAndHashCode
 @ToString
 @Getter
 @Setter
+@Entity
+@Data
 public class Vinyl {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String bandName;
+    @Column(name = "title", nullable = false)
     private String title;
+    @Column(name = "yearOfPublication", nullable = false)
     private Integer yearOfPublication;
+    @Column(name = "musicGenre", nullable = false)
     private MusicGenre musicGenre;
+    @Column(name = "sizeVinyl", nullable = false)
     private SizeVinyl sizeVinyl;
+
+    @ManyToOne
+    @JoinColumn(name = "cataloger_user_id")
+    private CatalogerUser catalogerUser;
 }
